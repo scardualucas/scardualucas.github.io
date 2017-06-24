ea0770a6c38de5279885fa2e5d1d122d57c9f20f
scardualucas <lcs.scardua@gmail.com>
2017-06-24 11:44:43 -0300

    jQuery removed, add JS

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,23 +1,48 @@
-$(document).ready(function() {
-    $(".btn-menu").click(function() {
-		if($(".menu").hasClass("closed")) {
-            if ($(window).width() > 1200) {
-                $(".menu").animate({height:"60px"},{queue:false, duration:200}).removeClass("closed").addClass("opened");
-            } else if ($(window).width() > 750) {
-                $(".menu").animate({height:"75px"},{queue:false, duration:200}).removeClass("closed").addClass("opened");
-            } else {
-			    $(".menu").animate({height:"50px"},{queue:false, duration:200}).removeClass("closed").addClass("opened");
-            }
+var btnMenu = document.querySelector(".btn-menu");
+var btnClose = document.querySelector(".btn-close");
+var menu = document.querySelector(".menu");
+
+var abreMenu = function() {    
+
+    if (menu.classList.contains("closed")) {
+
+        if (window.innerWidth > 1200) {
+
+            menu.style.height = "60px";
+            menu.classList.remove("closed");
+            menu.classList.add("opened");
+
+        } else if (window.innerWidth > 750) {
+            
+            menu.style.height = "75px";
+            menu.classList.remove("closed");
+            menu.classList.add("opened");
+
+        } else {
+
+            menu.style.height = "50px";
+            menu.classList.remove("closed");
+            menu.classList.add("opened");
+
         }
+    }
 
-        $(".btn-close").css("display", "flex");
-	});
+    btnClose.style.display = "flex";
+}
 
-    $(".btn-close").click(function() {
-		if($(".menu").hasClass("opened")) {
-			$(".menu").animate({height:"0px"},{queue:false, duration:200}).removeClass("opened").addClass("closed");
-        }
+var fechaMenu = function() {
 
-        $(".btn-close").hide();
-	}); 
-})        
+    if (menu.classList.contains("opened")) {
+
+        menu.style.height = "0px";
+        menu.classList.remove("opened");
+        menu.classList.add("closed");
+        
+    }
+
+    btnClose.style.display = "none";
+
+}
+
+btnMenu.addEventListener("click", abreMenu);
+btnClose.addEventListener("click", fechaMenu);
